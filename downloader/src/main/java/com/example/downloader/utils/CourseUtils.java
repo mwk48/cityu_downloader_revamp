21dfb04a9db9f329c995cbb9facc1d10da1b3823
Ma Wing Kin <mwk0048@gmail.com>
2023-04-18 23:39:58 +0800

    minor change

--- downloader/src/main/java/com/example/downloader/utils/CourseUtils.java
+++ downloader/src/main/java/com/example/downloader/utils/CourseUtils.java
@@ -6,14 +6,17 @@ import com.example.downloader.models.NameCourse;
 import com.example.downloader.repositories.CourseRepository;
 import com.example.downloader.repositories.GroupCourseRepository;
 import com.example.downloader.repositories.NameCourseRepository;
+import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Transactional
 public class CourseUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(CourseUtils.class);
+
 
     public static void insertAllCourses(List<Course> courses, CourseRepository courseRepository,
                                         GroupCourseRepository groupCourseRepository,
