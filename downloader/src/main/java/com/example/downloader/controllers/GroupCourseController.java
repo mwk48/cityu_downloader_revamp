fdc2aca543a50a052323b708f0208784414b8c45
Ma Wing Kin <mwk0048@gmail.com>
2023-04-22 20:11:01 +0800

    swagger for remaining routes

--- downloader/src/main/java/com/example/downloader/controllers/GroupCourseController.java
+++ downloader/src/main/java/com/example/downloader/controllers/GroupCourseController.java
@@ -1,8 +1,15 @@
 package com.example.downloader.controllers;
 
+import com.example.downloader.exceptions.ApiException;
 import com.example.downloader.models.Course;
 import com.example.downloader.models.GroupCourse;
 import com.example.downloader.services.GroupCourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,13 +28,29 @@ public class GroupCourseController {
         this.groupCourseService = groupCourseService;
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = Course.class)))),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ApiException.class))),
+    })
+    @Operation(summary = "Get courses by subject and year")
     @GetMapping(path = "/courses", params = {"subject", "year"})
 
-    public List<Course> getGroupCourses(@RequestParam("subject") String subject,
-                                        @RequestParam("year") String year) {
+    public List<Course> getGroupCourses(
+        @Schema(type = "string", example = "CS") @RequestParam("subject") String subject,
+        @Schema(type = "string", example = "1996-1997A") @RequestParam("year") String year) {
         return groupCourseService.getAllCoursesBySubjectAndYear(subject, year);
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = GroupCourse.class)))),
+    })
+    @Operation(summary = "Get all group courses")
     @GetMapping(path = "/all")
 
     public List<GroupCourse> getAllGroupCourses() {
@@ -35,24 +58,51 @@ public class GroupCourseController {
     }
 
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = String.class)))),
+    })
+    @Operation(summary = "Get subjects by year")
     @GetMapping(path = "/subjects", params = {"year"})
 
-    public List<String> getSubjects(@RequestParam("year") String year) {
+    public List<String> getSubjects(
+        @Schema(type = "string", example = "1996-1997A") @RequestParam(value = "year")
+        String year) {
         return groupCourseService.getSubjects(year);
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = String.class)))),
+    })
+    @Operation(summary = "Get years by subject")
     @GetMapping(path = "/years", params = {"subject"})
 
-    public List<String> getYears(@RequestParam("subject") String subject) {
+    public List<String> getYears(
+        @Schema(type = "string", example = "CS") @RequestParam("subject") String subject) {
         return groupCourseService.getYears(subject);
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = String.class)))),
+    })
+    @Operation(summary = "Get all subjects")
     @GetMapping(path = "/all/subjects")
 
     public List<String> getAllSubjects() {
         return groupCourseService.getAllSubjects();
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = String.class)))),
+    })
+    @Operation(summary = "Get all years")
     @GetMapping(path = "/all/years")
 
     public List<String> getAllYears() {
--- downloader/src/main/java/com/example/downloader/controllers/NameCourseController.java
+++ downloader/src/main/java/com/example/downloader/controllers/NameCourseController.java
@@ -1,8 +1,15 @@
 package com.example.downloader.controllers;
 
+import com.example.downloader.exceptions.ApiException;
 import com.example.downloader.models.Course;
 import com.example.downloader.models.NameCourse;
 import com.example.downloader.services.NameCourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +28,30 @@ public class NameCourseController {
         this.nameCourseService = nameCourseService;
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = Course.class)))),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ApiException.class))),
+    })
+    @Operation(summary = "Get courses by name")
     @GetMapping(path = "/courses", params = {"name"})
 
-    public List<Course> getGroupCourses(@RequestParam("name") String name) {
+    public List<Course> getGroupCourses(
+        @Schema(type = "string", example = "CS0242") @RequestParam("name") String name) {
         return nameCourseService.getAllCoursesByName(name);
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+            mediaType = "application/json",
+            array = @ArraySchema(schema = @Schema(implementation = NameCourse.class)))),
+    })
+    @Operation(summary = "Get all name courses")
     @GetMapping(path = "/all")
-
+    
     public List<NameCourse> getAllNameCourses() {
         return nameCourseService.getAllNameCourses();
     }
